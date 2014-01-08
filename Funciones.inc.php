a1f61c41b03a7ff3e8ae90f60bebbf2f0bce812f
Byron Peña <byronpenna@gmail.com>
2014-01-07 21:15:19 -0600

    chance

--- Funciones.inc.php
+++ Funciones.inc.php
@@ -1,8 +1,35 @@
 <?php 
+$func = new Funciones();
+$func->valExpression("2+5+3-5+8-2*5");
 class Funciones
 {
 	function valExpression($expresion){
-		echo "Byron es marica.";
+		$symbols = array("+");
+		$var = array();
+		$cn = 0;
+		foreach ($symbols as $key => $value) {
+			$arr = explode($value,$expresion);
+			echo "<br>";
+			print_r($arr);
+		}
+		foreach ($arr as $key => $value) {
+			if(is_numeric($value)){
+				$final[$cn] = $value;
+				$final[$cn+1] = "+";
+				$cn += 2;
+			}
+		}
+		if(!is_numeric(array_pop($final))){
+			array_push($final,"");
+		}
+		
+		echo "<br>otro<br>";
+		print_r($arr);
+		echo "<br>final<br>";
+		print_r($final);
+		// while (strpos($expresion, needle)) {
+		// 	# code...
+		// }
 	}
 }
 ?>
