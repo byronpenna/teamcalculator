8dd0ce9b23e3759970be413f4d9f80e29f39a48d
Byron Peña <byronpenna@gmail.com>
2014-01-08 20:16:31 -0600

    almost

--- Funciones.inc.php
+++ Funciones.inc.php
@@ -4,32 +4,33 @@ $func->valExpression("2+5+3-5+8-2*5");
 class Funciones
 {
 	function valExpression($expresion){
-		$symbols = array("+");
-		$var = array();
+		$simbolos = array('+','-','/','*');
+		foreach ($simbolos as $llave => $valor) {
+			
+		}
+		$arr = explode($valor, $expresion);
 		$cn = 0;
-		foreach ($symbols as $key => $value) {
-			$arr = explode($value,$expresion);
-			echo "<br>";
-			print_r($arr);
-		}
+		$i = 0;
 		foreach ($arr as $key => $value) {
-			if(is_numeric($value)){
-				$final[$cn] = $value;
-				$final[$cn+1] = "+";
-				$cn += 2;
+			$result = $cn % 2;
+			if($result == 0 && is_numeric($value)){
+				echo "||$value||";
+				$arrSum[$i] = $value;
+				$arr[$i] = "";
+			}else if($result == 1 && is_numeric($value)){
+				$i++;
+				$arrSum[$i] = $valor;
+				$arr[$i] = "";
+				$i++;
+				$arrSum[$i] = $value;
 			}
+			$cn ++;
+			// echo "<br>".$result."<br>"; 
 		}
-		if(!is_numeric(array_pop($final))){
-			array_push($final,"");
-		}
-		
-		echo "<br>otro<br>";
+		$arr = array_filter($arr);
 		print_r($arr);
-		echo "<br>final<br>";
-		print_r($final);
-		// while (strpos($expresion, needle)) {
-		// 	# code...
-		// }
+		echo "<br>";
+		print_r($arrSum);
 	}
 }
 ?>
